259647ad5a0756dd62e373205e0af3e2b38c851e
Miguel Angel Cañas <macvaz82@gmail.com>
2012-11-26 16:17:27 +0100

    managing database_url setting properly

--- acquisition/settings.py
+++ acquisition/settings.py
@@ -194,8 +194,9 @@ LOGGING = {
     }
 }
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
 
 BROKER_BACKEND = 'django'
 
