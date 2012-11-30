e91a34d1622e8c857bd98419ebcdf4e464c574b6
Miguel Angel Cañas <macvaz82@gmail.com>
2012-11-30 19:41:10 +0100

    deleting files

--- tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from invoicer.customer.salesforce import getCatalogue
-
-class TestSalesForce(unittest.TestCase):
-    def testDownloadCatalogue(self):
-        getCatalogue()
-        
